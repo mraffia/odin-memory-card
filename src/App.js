794fb7545d405501649df50d5cc3ddd71bb83e1a
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-10-27 00:52:18 +0700

    Add useEffect logic to randomize the cards placement at the first render, everytime the player clicks a card, and when the game resets

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Card from './components/Card.js';
 import './App.css';
 
 function App() {
-  const initialCards = [
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [cards, setCards] = useState([
     ["bretta", false],
     ["brumm", false],
     ["cloth", false],
@@ -16,11 +18,7 @@ function App() {
     ["sly", false],
     ["tiso", false],
     ["zote", false],
-  ];
-
-  const [score, setScore] = useState(0);
-  const [bestScore, setBestScore] = useState(0);
-  const [cards, setCards] = useState(initialCards);
+  ]);
 
   function shuffle(arr) {
     let currentIndex = arr.length, randomIndex;
@@ -36,6 +34,16 @@ function App() {
     return shuffled;
   }
 
+  function resetStatus(arr) {
+    let reset = arr.slice();
+
+    for (let i = 0; i < reset.length; i++) {
+      reset[i].splice(1, 1, false);
+    }
+
+    return reset;
+  }
+
   function handleClick(card, i) {
     let clicked = cards.slice();
 
@@ -44,8 +52,9 @@ function App() {
       setCards(clicked);
       setScore(score + 1);
     } else if (clicked[i][1] === true) {
+      const reset = resetStatus(cards)
+      setCards(reset);
       setScore(0);
-      setCards(initialCards);
     }
   }
 
@@ -54,13 +63,20 @@ function App() {
       setBestScore(score);
     }
 
+    const shuffled = shuffle(cards);
+    setCards(shuffled);
+  }, [score]);
+
+  useEffect(() => {
     if (bestScore === 12) {
       window.confirm("Congrats, you beat the game! (Press any button to restart)");
+      
+      const reset = resetStatus(cards)
+      setCards(reset);
       setScore(0);
       setBestScore(0);
-      setCards(initialCards);
     }
-  });
+  }, [bestScore]);
   
   return (
     <div className="container">
