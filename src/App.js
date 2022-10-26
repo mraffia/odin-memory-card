014e8e2b9fd7f5899fddaef8c0c66b3efd47ee80
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-10-26 23:06:44 +0700

    Add shuffle function

--- src/App.js
+++ src/App.js
@@ -20,6 +20,25 @@ function App() {
     ["zote", false],
   ]);
 
+  // Source: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  function shuffle(arr) {
+    let currentIndex = arr.length, randomIndex;
+    let shuffled = arr.slice();
+  
+    // While there remain elements to shuffle.
+    while (currentIndex !== 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [shuffled[currentIndex], shuffled[randomIndex]] = [shuffled[randomIndex], shuffled[currentIndex]];
+    }
+  
+    return shuffled;
+  }
+
   return (
     <div className="container">
       <div className="header">
