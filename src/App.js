b1a4a315db0ca586550fd4aa2a9f82699ea8e57a
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-10-27 00:08:36 +0700

    Add useEffect to record the best score and reset the game if the score reach 12 points

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import Card from './components/Card.js';
 import './App.css';
 
 function App() {
-  const [score, setScore] = useState(0);
-  const [bestScore, setBestScore] = useState(0);
-  const [cards, setCards] = useState([
+  const initialCards = [
     ["bretta", false],
     ["brumm", false],
     ["cloth", false],
@@ -18,7 +16,11 @@ function App() {
     ["sly", false],
     ["tiso", false],
     ["zote", false],
-  ]);
+  ];
+
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [cards, setCards] = useState(initialCards);
 
   function shuffle(arr) {
     let currentIndex = arr.length, randomIndex;
@@ -39,33 +41,27 @@ function App() {
 
     if (clicked[i][1] === false) {
       clicked[i].splice(1, 1, true);
-      setScore(score + 1);
       setCards(clicked);
+      setScore(score + 1);
     } else if (clicked[i][1] === true) {
       setScore(0);
-      setCards([
-        ["bretta", false],
-        ["brumm", false],
-        ["cloth", false],
-        ["grimm", false],
-        ["hornet", false],
-        ["jiji", false],
-        ["knight", false],
-        ["quirrel", false],
-        ["seer", false],
-        ["sly", false],
-        ["tiso", false],
-        ["zote", false],
-      ]);
+      setCards(initialCards);
     }
-    
-    console.log(cards);
   }
 
   useEffect(() => {
-    console.log(cards);
-  });
+    if (bestScore < score) {
+      setBestScore(score);
+    }
 
+    if (bestScore === 12) {
+      window.confirm("Congrats, you beat the game! (Press any button to restart)");
+      setScore(0);
+      setBestScore(0);
+      setCards(initialCards);
+    }
+  });
+  
   return (
     <div className="container">
       <div className="header">
