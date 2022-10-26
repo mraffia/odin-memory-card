fa8c7d9bc7adcc6af7cfa08fe4de276fdebfffc6
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-10-26 19:07:13 +0700

    Add header and footer and its stylings

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
   return (
-    <div className="App">
+    <div className="container">
+      <div className="header">
+        <div className="header-title">Odin Memory Card Game</div>
+      </div>
+
+      <div className="cards-container">
+
+      </div>
+
+      <div className="footer">
+        By yours truly,&nbsp;<a href="https://github.com/mraffia"> <strong>mraffia</strong></a>
+      </div>
     </div>
   );
 }
